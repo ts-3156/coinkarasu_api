6f73f58ead91274d0ec40e65d01c025b2ed88ad6
ts-3156 <ts_3156@yahoo.co.jp>
2018-01-12 04:34:13 +0900

    Add constraints: {format: 'json'}

--- app/controllers/coincheck/sales_rates_controller.rb
+++ app/controllers/coincheck/sales_rates_controller.rb
@@ -9,9 +9,9 @@ class Coincheck::SalesRatesController < ApplicationController
     from_symbol = params[:from_symbol]
     to_symbol = params[:to_symbol]
 
-    return render json: {} if time.blank? || !time.match(/\A\d+\z/) ||
-        from_symbol.blank? || !from_symbol.match(/\A[A-Z]{10}\z/) ||
-        to_symbol.blank? || !to_symbol.match(/\A[A-Z]{10}\z/)
+    return render json: {} if time.blank? || !time.match?(/\A\d+\z/) ||
+        from_symbol.blank? || !from_symbol.match?(/\A[A-Z]{10}\z/) ||
+        to_symbol.blank? || !to_symbol.match?(/\A[A-Z]{10}\z/)
 
     rate = Coincheck::SalesRate.order(created_at: :desc)
                .where('created_at < ?', Time.zone.at(time.to_i))
--- app/models/security.rb
+++ app/models/security.rb
@@ -11,8 +11,8 @@ class Security
       nonce = request.headers[:ACCESS_NONCE]
       signature = request.headers[:ACCESS_SIGNATURE]
 
-      return false if (key.blank? || nonce.blank? || signature.blank?)
-      return false if !nonce.match?(/\A\d+\z/) || 1.minute.ago > nonce.to_i
+      return false if key.blank? || nonce.blank? || signature.blank?
+      return false if !nonce.match?(/\A\d+\z/) || 1.minute.ago < nonce.to_i
 
       app = App.find_by(key: key)
       return false unless app
